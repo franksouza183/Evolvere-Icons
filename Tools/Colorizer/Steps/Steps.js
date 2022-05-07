cad943d45a67b3eaf5d222126fe20f6b5a99aec3
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-05-07 05:45:56 -0400

    Added Monochrome Selection List

--- Tools/Colorizer/Steps/Steps.js
+++ Tools/Colorizer/Steps/Steps.js
@@ -1,8 +1,9 @@
 
+import selection from './Selection.js'
 import template from './Template.js'
 import colorize from './Colorize.js'
 import search from './Search.js'
 import cache from './Cache.js'
 import copy from './Copy.js'
 
-export default { copy , search , colorize , template , cache };
+export default { copy , search , colorize , template , cache , selection };
